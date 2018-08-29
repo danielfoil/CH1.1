7b38d504e347a9399ffa351e81947a5289f51095
danielfoil <37551670+danielfoil@users.noreply.github.com>
2018-08-29 17:05:38 +0200

    added spaces and line breaks

--- hello.py
+++ hello.py
@@ -2,9 +2,13 @@
 print( 'Hello world!')
 print( 'What is your name?') #ask for their name
 myName = input()
-print( 'It is good to meet you,' + myName)
+print()
+print( 'It is good to meet you, ' + myName)
+print()
 print( 'The length of your name is: ')
 print(len(myName))
+print()
 print('What is your age?') #ask for their age
 myAge = input()
+print()
 print('You will be ' + str(int(myAge) + 1) + ' in a year.')
